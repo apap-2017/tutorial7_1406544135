879c218bc31d99e4d322d8d94b0528d132f0cd91
GraciaGit\Gitta Garcia Evelyn <gittagraciaevelyndiva@gmail.com>
2017-11-04 20:17:08 +0700

    latihan 3

---
+++ Tutorial07Consumer/src/main/java/com/example/demo/dao/StudentDAO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.model.StudentModel;
+
+public interface StudentDAO {
+	StudentModel selectStudent(String npm);
+	List<StudentModel> selectAllStudents();
+}
---
+++ Tutorial07Consumer/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.CourseModel;
+import com.example.demo.model.StudentModel;
+
+public interface StudentService
+{
+    StudentModel selectStudent (String npm);
+    
+    CourseModel selectCourse (String id);
+
+    List<StudentModel> selectAllStudents ();
+
+
+    void addStudent (StudentModel student);
+
+    void updateStudent (StudentModel student);
+
+    void deleteStudent (String npm);
+}
